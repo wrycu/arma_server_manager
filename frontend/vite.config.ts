583888f02068c74aea77ae787f8486711a10edf4
Britton Hayes <brittonhayes@live.com>
2025-08-05 22:37:51 -0700

    feat: simple cron helper and tests (#20)
    Co-authored-by: Britton Hayes <brittonhayes@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
@@ -19,5 +19,9 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
 })
