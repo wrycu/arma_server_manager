9aad1a24b91b2954b187d0e9434f0d3ae74b900a
Britton Hayes <brittonhayes@users.noreply.github.com>
2025-09-27 10:44:00 -0700

    refactor: clean up service exports in index.ts

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -3,11 +3,9 @@ import { serverService } from './server.service'
 import { modService } from './mods.service'
 import { scheduleService } from './schedules.service'
 
-// Export real services only
 export const collections = collectionsService
 export const server = serverService
 export const mods = modService
 export const schedules = scheduleService
 
-// Re-export the API service for direct use
 export { api } from './api'
