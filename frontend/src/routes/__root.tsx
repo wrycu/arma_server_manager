3c8d40c03251bfa9e5918ba0a9b629e14b6bfa27
Britton Hayes <brittonhayes@users.noreply.github.com>
2025-09-28 09:31:51 -0700

    design: new arma reforger inspired design refresh

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -7,22 +7,27 @@ import { Toaster } from '@/components/ui/sonner'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <div className="@container/main flex flex-1 flex-col gap-4">
-            <div className="flex flex-col gap-6 py-12 md:gap-8 md:py-14">
-              <div className="px-6 lg:px-8">
-                <Outlet />
+    <div className="relative min-h-screen">
+      {/* Arma-style topographic background overlay */}
+      <div className="arma-topographic-background" />
+
+      <div className="relative z-10">
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <div className="@container/main flex flex-1 flex-col gap-4">
+              <div className="flex flex-col gap-6 py-12 md:gap-8 md:py-14">
+                <div className="px-6 lg:px-8">
+                  <Outlet />
+                </div>
               </div>
             </div>
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
-      <NavigationCommandDialog />
-      <Toaster />
-      <TanStackRouterDevtools />
-    </>
+          </SidebarInset>
+        </SidebarProvider>
+        <NavigationCommandDialog />
+        <Toaster />
+        <TanStackRouterDevtools />
+      </div>
+    </div>
   ),
 })
